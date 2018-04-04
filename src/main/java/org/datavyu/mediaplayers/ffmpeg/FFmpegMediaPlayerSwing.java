f1ee4d81ee03729cfd8b171951dcffdf909e2c68
Reda Nezzar <nezzar.reda@gmail.com>
2018-04-04 13:50:41 -0400

    DVD-216: FFmpeg Media Player, play and stop(Swing)

--- src/main/java/org/datavyu/mediaplayers/ffmpeg/FFmpegMediaPlayerSwing.java
+++ src/main/java/org/datavyu/mediaplayers/ffmpeg/FFmpegMediaPlayerSwing.java
@@ -1,39 +1,61 @@
 package org.datavyu.mediaplayers.ffmpeg;
 
-
+import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.bytedeco.javacv.*;
 import org.datavyu.madias.DatavyuMedia;
 import org.datavyu.mediaplayers.StreamViewer;
 import org.datavyu.util.Identifier;
 import org.datavyu.util.Rate;
 
-import javax.swing.*;
+import java.io.File;
 
 
 /**
  * This is an FFmpeg Media Playerd displayed in a Swing Frame
  */
-public class FFmpegMediaPlayerSwing implements StreamViewer{
+public class FFmpegMediaPlayerSwing implements StreamViewer {
     private final Identifier identifier;
     private DatavyuMedia media;
+    private CanvasFrame jFrame;
+    private FFmpegFrameGrabber grabber;
+    private FFmpegFrameRecorder recorder;
 
     //TODO: add a frame grabber and an audio grabber and a MovieStreamer to sync the two streams
     // FFmpeg Media Player will talk to the MovieStreamer
 
-
-    public FFmpegMediaPlayerSwing(Identifier identifier, DatavyuMedia media) {
+    public FFmpegMediaPlayerSwing(Identifier identifier, DatavyuMedia media){
         this.identifier = identifier;
         this.media = media;
+        jFrame = new CanvasFrame(new File("/Users/redanezzar/Databrary/datavyu-incubator/src/main/resources/DatavyuSampleVideo.mp4").toString());
+        try {
+            grabber = FFmpegFrameGrabber.createDefault(new File("/Users/redanezzar/Databrary/datavyu-incubator/src/main/resources/DatavyuSampleVideo.mp4"));
+            grabber.start();
+//            recorder = new FFmpegFrameRecorder("DatavyuSampleVideo.avi", grabber.getImageWidth(), grabber.getImageHeight());
+        } catch (FrameGrabber.Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
     public Identifier getIdentifier() {
-        return null;
+        return this.identifier;
     }
 
     @Override
     public void play() {
-
+        try {
+            if(grabber.grab() != null){
+                jFrame.showImage(grabber.grab());
+//                recorder.record(grabber.grab());
+            }
+        } catch (FrameGrabber.Exception e) {
+            e.printStackTrace();
+        }
+//        catch (FrameRecorder.Exception e) {
+//            e.printStackTrace();
+//        }
     }
 
     @Override
@@ -43,7 +65,12 @@ public class FFmpegMediaPlayerSwing implements StreamViewer{
 
     @Override
     public void stop() {
-
+        try {
+            grabber.stop();
+            grabber.release();
+        } catch (FrameGrabber.Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
